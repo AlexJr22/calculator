afeaadcb42bb29daeb0b97f6b416951ed5332752
Alex Junior <alexjr2oficial@gmail.com>
2022-10-09 12:19:42 -0300

    creating backspace.js

---
+++ calculator/scripts/BackSpace.js
@@ -0,0 +1,7 @@
+function backSpace(){
+  displayCalculator.innerHTML = displayCalculator.innerHTML.substring(0, displayCalculator.innerHTML.length -1)
+}
+
+function clearCharacters(){
+  displayCalculator.innerHTML = ''
+}
