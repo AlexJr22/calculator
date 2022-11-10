c635354e596a6ff16318039b7111eaf0d8b7912e
Alex Junior <alexjr2oficial@gmail.com>
2022-11-10 11:31:47 -0300

    update operators.js

--- calculator/scripts/operators.js
+++ calculator/scripts/operators.js
@@ -1,17 +1,29 @@
 function insertOperators(num){
-  const lastOperator = displayCalculator.innerHTML[displayCalculator.innerHTML.length - 1]
+
+  const lastOperator = displayCalculator.innerHTML[displayCalculator.innerHTML.length - 1];
+  
   if (lastOperator == '+'|| lastOperator == '-' || lastOperator == '*' || lastOperator == "/") {
+
     displayCalculator.innerHTML = displayCalculator.innerHTML.substring(0, displayCalculator.innerHTML.length -1)
+
     displayCalculator.innerHTML += num
-  }else{
+
+  } else{
+
     displayCalculator.innerHTML += num
+
   }
 }
 
 function result(){
+
   if(eval(displayCalculator.innerHTML) == undefined){
+
     displayCalculator.innerHTML += ''
+
   }else{
+
     displayCalculator.innerHTML = eval(displayCalculator.innerHTML)
+
   }
 }
