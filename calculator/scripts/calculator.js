2d34383b5feb078e43ec21610ca5a4a34ab8ed7c
Alex Junior <alexjr2oficial@gmail.com>
2022-11-11 12:41:05 -0300

    compiling the TypeScript file

---
+++ calculator/scripts/calculator.js
@@ -0,0 +1,28 @@
+const displayCalculator = document.querySelector('.displayCalculator');
+function insertNumbers(num) {
+    displayCalculator.innerHTML += num;
+}
+function backSpace() {
+    displayCalculator.innerHTML = displayCalculator.innerHTML.substring(0, displayCalculator.innerHTML.length - 1);
+}
+function clearCharacters() {
+    displayCalculator.innerHTML = '';
+}
+function insertOperators(num) {
+    const lastOperator = displayCalculator.innerHTML[displayCalculator.innerHTML.length - 1];
+    if (lastOperator == '+' || lastOperator == '-' || lastOperator == '*' || lastOperator == "/") {
+        displayCalculator.innerHTML = displayCalculator.innerHTML.substring(0, displayCalculator.innerHTML.length - 1);
+        displayCalculator.innerHTML += num;
+    }
+    else {
+        displayCalculator.innerHTML += num;
+    }
+}
+function result() {
+    if (eval(displayCalculator.innerHTML) !== undefined) {
+        displayCalculator.innerHTML = eval(displayCalculator.innerHTML);
+    }
+    else {
+        displayCalculator.innerHTML += '';
+    }
+}
